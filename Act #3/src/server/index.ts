927122c88f45ca2b8fe8e036f947e05548746bf3
Dakrfox <business.nimh@outlook.com>
2023-07-16 08:37:21 -0500

    act #3

---
+++ Act #3/src/server/index.ts
@@ -0,0 +1,44 @@
+import express, { Express, Request, Response } from "express";
+
+// Security
+import cors from 'cors';
+import helmet from 'helmet';
+
+// TODO: HTTPS
+
+// Root Router
+import rootRuter from '../routes';
+
+// * Create Express APP
+const server: Express = express();
+
+
+// * Define SERVER to use "/api" and use rootRouter from 'index.ts' in routes
+// From this point onover: http://localhost:8000/api/...
+server.use(
+    '/api',
+    rootRuter
+    );
+
+
+// Static server
+server.use(express.static('public'));
+
+// TODO: Mongoose Connection
+
+// * Security Config
+server.use(helmet());
+server.use(cors());
+
+// * Content Type Config
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({limit: '50mb'}));
+
+// * Redirection Config
+// http://localhost:8000/ --> http://localhost:8000/api/
+server.get('/', (req: Request, res: Response) => {
+    res.redirect('/api');
+});
+
+export default server;
+
